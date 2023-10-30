1e845658a7cf5c401024566d6e4adc7a90e4ead1
laxmi <laxmiyadav21@navgurukul.org>
2023-10-30 16:30:17 +0530

    usecontext

--- src/Usecontext/Child.js
+++ src/Usecontext/Child.js
@@ -1,11 +1,20 @@
-import React from "react";
-const Child=()=>{
-    return(
-        <>
-            <div>
-                <h1>use context child</h1>
-            </div>
-        </>
-    )
-}
-export default  Child
+import React, { useContext } from "react";
+import { GlobalInfo } from "./Usecontext";
+
+const Child = () => {
+  const { appColor } = useContext(GlobalInfo);
+  console.warn("appColor", appColor);
+
+  return (
+    <>
+      <div>
+        <h1>use context child</h1>
+        <div style={{ backgroundColor: appColor }}>
+          This div's background color is determined by the context value.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Child;
--- src/Usecontext/Usecontext.js
+++ src/Usecontext/Usecontext.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import Child from "./Child";
-const Usecontext=()=>{
-    return(
-        <>
-            <div>
-                <h1>use context</h1>
-                <Child/>
-            </div>
-        </>
-    )
-}
-export default  Usecontext
+
+export const GlobalInfo = createContext();
+
+const Usecontext = () => {
+  const [color, setColor] = useState("green");
+
+  return (
+    <GlobalInfo.Provider value={{ appColor: color }}>
+      <div>
+        <h1>use context</h1>
+        <Child />
+      </div>
+    </GlobalInfo.Provider>
+  );
+};
+
+export default Usecontext;
